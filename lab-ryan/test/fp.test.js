8da7963904892afe82f792ca437df7877c64bdc5
rjtj2007 <teacherman1@gmail.com>
2018-03-01 22:43:49 -0800

    added tests for normal cases and for all functions and all pass

--- lab-ryan/test/fp.test.js
+++ lab-ryan/test/fp.test.js
@@ -1,13 +1,39 @@
 'use strict';
 
-
 const Fp = require('../lib/fp.js');
 
 // .forEach
-describe('Fp.forEach with null as arr', () => {
-  let expected = [];
-  it('it should return undefined', () => {
+describe('Fp.forEach with null as array', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
     let arr = null;
+    let result = undefined;
+    let cb = (something) => {
+      return;
+    }
+    Fp.forEach(arr, (something) => {
+      result.push(cb(something));
+    });
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.forEach with null entry for arr', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
+    let arr = null;
+    let cb = (something) => {
+      return `New something: ${something + 1}`;
+    }
+    let result = Fp.forEach(arr, cb);
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.forEach test array with one item', () => {
+  let expected = [2];
+  test('it should return 2', () => {
+    let arr = [1];
     let result = [];
     let cb = (something) => {
       return something * 2;
@@ -20,39 +46,123 @@ describe('Fp.forEach with null as arr', () => {
 });
 
 // .map
+describe('Fp.map with null as array', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
+    let arr = null;
+    let result = undefined;
+    let cb = (something) => {
+      return;
+    }
+    Fp.map(arr, (something) => {
+      result.push(cb(something));
+    });
+    expect(result).toBe(expected);
+  })
+});
+
 describe('Fp.map with null entry for arr', () => {
   let expected = undefined;
-  it('it should return undefined', () => {
+  test('it should return undefined', () => {
     let arr = null;
     let cb = (something) => {
       return `New something: ${something + 1}`;
     }
     let result = Fp.map(arr, cb);
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.map test array with one item', () => {
+  let expected = [2];
+  test('it should return 2', () => {
+    let arr = [1];
+    let result = [];
+    let cb = (something) => {
+      return something * 2;
+    }
+    Fp.map(arr, (something) => {
+      result.push(cb(something));
+    });
     expect(result).toEqual(expected);
   })
 });
 
 //.filter
-describe('Fp.filter with empty arr', () => {
+describe('Fp.map with null as array', () => {
   let expected = undefined;
-  it('it should return undefined', () => {
-    let arr = [];
+  test('it should return undefined', () => {
+    let arr = null;
+    let result = undefined;
     let cb = (something) => {
-      if (something > 2) {
-        return true;
-      } 
-        return false;
+      return;
+    }
+    Fp.map(arr, (something) => {
+      result.push(cb(something));
+    });
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.filter with null entry for arr', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
+    let arr = null;
+    let cb = (something) => {
+      return `New something: ${something + 1}`;
     }
     let result = Fp.filter(arr, cb);
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.filter test array with one item', () => {
+  let expected = [2];
+  test('it should return 2', () => {
+    let arr = [1];
+    let result = [];
+    let cb = (something) => {
+      return something * 2;
+    }
+    Fp.filter(arr, (something) => {
+      result.push(cb(something));
+    });
     expect(result).toEqual(expected);
   })
 });
 
 //.reduce
-describe('Fp.reduce test arr with one item', () => {
-  let expected = 1;
-  it('it should return the same thing', () => {
-    let arr = [1];
+describe('Fp.reduce with null as array', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
+    let arr = null;
+    let result = undefined;
+    let cb = (something) => {
+      return;
+    }
+    Fp.reduce(arr, (something) => {
+      result.push(cb(something));
+    });
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.reduce with null entry for arr', () => {
+  let expected = undefined;
+  test('it should return undefined', () => {
+    let arr = null;
+    let cb = (something) => {
+      return `New something: ${something + 1}`;
+    }
+    let result = Fp.reduce(arr, cb);
+    expect(result).toBe(expected);
+  })
+});
+
+describe('Fp.reduce test array with one item', () => {
+  let expected = 4;
+  test('it should return the same thing', () => {
+    let arr = [4];
     let cb = (accumulator, something) => {
       return accumulator + something;
     }
